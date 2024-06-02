5b9fb5febc7031071c1dc45c205d68bd16d40b3c
Ce1Y <sycn02430@gmail.com>
2024-06-02 21:00:29 +0800

    Add showing suggestions function

--- flutter_finalproject/lib/searchAnchor.dart
+++ flutter_finalproject/lib/searchAnchor.dart
@@ -19,7 +19,8 @@ class _AsyncSearchAnchorState extends State<AsyncSearchAnchor> {
   // The query currently being searched for. If null, there is no pending
   // request.
   String? _searchingWithQuery;
-  late List<Character> _characters;
+  late List<Character> _futureCharacters;
+  List<Character> _suggestionCharacters = [];
 
   // The most recent options received from the API.
   late Iterable<Widget> _lastOptions = <Widget>[];
@@ -30,12 +31,22 @@ class _AsyncSearchAnchorState extends State<AsyncSearchAnchor> {
     getCharacterList();
   }
 
+  // Convert future type list to common list
   Future<List<Character>> convertListType() {
     return widget.futureCharacters;
   }
 
   void getCharacterList() async {
-    _characters = await convertListType();
+    _futureCharacters = await convertListType();
+  }
+
+  void fetchSuggestionsList(String keyword) {
+    _suggestionCharacters.clear();
+    for (var character in _futureCharacters) {
+      if (character.name.toLowerCase().contains(keyword.toLowerCase())) {
+        _suggestionCharacters.add(character);
+      }
+    }
   }
 
   @override
@@ -44,81 +55,110 @@ class _AsyncSearchAnchorState extends State<AsyncSearchAnchor> {
       body: SafeArea(
         child: Align(
           alignment: Alignment.topCenter,
-          child: SearchAnchor(
-            builder: (BuildContext context, SearchController controller) {
-              return SearchBar(
-                leading: IconButton(
-                  icon: const Icon(Icons.arrow_back),
-                  onPressed: () {
-                    // If this widget return another materialApp, it'll return a black screen.
-                    Navigator.pop(context);
-                  },
-                ),
-                controller: controller,
-                hintText: 'Search character',
-                shape: MaterialStateProperty.all(
-                  const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-                ),
-                // onChanged: (value) => {controller.openView()},
-                onChanged: (value) => {print(value)},
-                elevation: MaterialStateProperty.all(0),
-                autoFocus: true,
-                textInputAction: TextInputAction.search,
-                onSubmitted: (value) {
-                  // print(value);
-                  bool isExist = false;
-                  late var charac;
-                  for (var character in _characters) {
-                    // if (character.name.contains(value)) {
-                    if (character.name.toLowerCase() == value.toLowerCase()) {
-                      isExist = true;
-                      charac = character;
-                      break;
-                    }
-                  }
-
-                  if (isExist) {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            CharacterDetailTile(character: charac),
-                      ),
-                    );
-                  }
-                  setState(() {});
-                },
-              );
-            },
-            suggestionsBuilder:
-                (BuildContext context, SearchController controller) async {
-              print(controller.text);
-              _searchingWithQuery = controller.text;
-              final List<String> options =
-                  (await _FakeAPI.search(_searchingWithQuery!)).toList();
-
-              // If another search happened after this one, throw away these options.
-              // Use the previous options instead and wait for the newer request to
-              // finish.
-              if (_searchingWithQuery != controller.text) {
-                return _lastOptions;
-              }
-
-              _lastOptions = List<ListTile>.generate(
-                options.length,
-                (int index) {
-                  final String item = options[index];
-                  return ListTile(
-                    title: Text(item),
-                    onTap: () {
-                      print("tapped");
+          child: Column(
+            children: [
+              SearchAnchor(
+                builder: (BuildContext context, SearchController controller) {
+                  // Search bar
+                  return SearchBar(
+                    leading: IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      onPressed: () {
+                        // If this widget return another materialApp, it'll return a black screen.
+                        Navigator.pop(context);
+                      },
+                    ),
+                    controller: controller,
+                    hintText: 'Search character',
+                    shape: MaterialStateProperty.all(
+                      const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero),
+                    ),
+                    // onChanged: (value) => {controller.openView()},
+                    onChanged: (value) => {
+                      setState(() {
+                        fetchSuggestionsList(value);
+                      }),
+                    },
+                    elevation: MaterialStateProperty.all(0),
+                    autoFocus: true,
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (value) {
+                      // print(value);
+                      bool isExist = false;
+                      late Character navigateCharacter;
+                      for (var character in _futureCharacters) {
+                        // if (character.name.contains(value)) {
+                        if (character.name.toLowerCase() ==
+                            value.toLowerCase()) {
+                          isExist = true;
+                          navigateCharacter = character;
+                          break;
+                        }
+                      }
+                      if (isExist) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CharacterDetailTile(
+                                character: navigateCharacter),
+                          ),
+                        );
+                      }
+                      // setState(() {});
                     },
                   );
                 },
-              );
+                suggestionsBuilder:
+                    (BuildContext context, SearchController controller) async {
+                  print(controller.text);
+                  _searchingWithQuery = controller.text;
+                  final List<String> options =
+                      (await _FakeAPI.search(_searchingWithQuery!)).toList();
 
-              return _lastOptions;
-            },
+                  // If another search happened after this one, throw away these options.
+                  // Use the previous options instead and wait for the newer request to
+                  // finish.
+                  if (_searchingWithQuery != controller.text) {
+                    return _lastOptions;
+                  }
+
+                  _lastOptions = List<ListTile>.generate(
+                    options.length,
+                    (int index) {
+                      final String item = options[index];
+                      return ListTile(
+                        title: Text(item),
+                        onTap: () {
+                          print("tapped");
+                        },
+                      );
+                    },
+                  );
+
+                  return _lastOptions;
+                },
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _suggestionCharacters.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(_suggestionCharacters[index].name),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CharacterDetailTile(
+                                character: _suggestionCharacters[index]),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
