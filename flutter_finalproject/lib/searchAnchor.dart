134bd75c3c974dc342b56deb061c0f8520707a1c
Ce1Y <sycn02430@gmail.com>
2024-06-10 20:59:41 +0800

    Update searchAnchor.dart

--- flutter_finalproject/lib/searchAnchor.dart
+++ flutter_finalproject/lib/searchAnchor.dart
@@ -16,14 +16,9 @@ class AsyncSearchAnchor extends StatefulWidget {
 }
 
 class _AsyncSearchAnchorState extends State<AsyncSearchAnchor> {
-  // The query currently being searched for. If null, there is no pending
-  // request.
   String? _searchingWithQuery;
   late List<Character> _futureCharacters;
   List<Character> _suggestionCharacters = [];
-
-  // The most recent options received from the API.
-  late Iterable<Widget> _lastOptions = <Widget>[];
 
   @override
   initState() {
@@ -110,32 +105,6 @@ class _AsyncSearchAnchorState extends State<AsyncSearchAnchor> {
                 },
                 suggestionsBuilder:
                     (BuildContext context, SearchController controller) async {
-                  // print(controller.text);
-                  // _searchingWithQuery = controller.text;
-                  // final List<String> options =
-                  //     (await _FakeAPI.search(_searchingWithQuery!)).toList();
-
-                  // // If another search happened after this one, throw away these options.
-                  // // Use the previous options instead and wait for the newer request to
-                  // // finish.
-                  // if (_searchingWithQuery != controller.text) {
-                  //   return _lastOptions;
-                  // }
-
-                  // _lastOptions = List<ListTile>.generate(
-                  //   options.length,
-                  //   (int index) {
-                  //     final String item = options[index];
-                  //     return ListTile(
-                  //       title: Text(item),
-                  //       onTap: () {
-                  //         print("tapped");
-                  //       },
-                  //     );
-                  //   },
-                  // );
-
-                  // return _lastOptions;
                   return [];
                 },
               ),
@@ -163,25 +132,5 @@ class _AsyncSearchAnchorState extends State<AsyncSearchAnchor> {
         ),
       ),
     );
-  }
-}
-
-// Mimics a remote API.
-class _FakeAPI {
-  static const List<String> _kOptions = <String>[
-    'aardvark',
-    'bobcat',
-    'chameleon',
-  ];
-
-  // Searches the options, but injects a fake "network" delay.
-  static Future<Iterable<String>> search(String query) async {
-    await Future<void>.delayed(fakeAPIDuration); // Fake 1 second delay.
-    if (query == '') {
-      return const Iterable<String>.empty();
-    }
-    return _kOptions.where((String option) {
-      return option.contains(query.toLowerCase());
-    });
   }
 }
